d8c01f1ec34cb1521a983036ff733641c336f97b
pterro2020 <154668100+pterro2020@users.noreply.github.com>
2025-03-02 21:53:59 +0300

    Update zap-script.js

--- ZAP/zap-script.js
+++ ZAP/zap-script.js
@@ -1,10 +1,10 @@
 // Пример скрипта для ZAP
-var targetUrl = "http://89.169.164.174; // Замените на URL вашего приложения
+var targetUrl = "http://89.169.164.174"; // Замените на URL вашего приложения
 var scanPolicyName = "Default Policy"; // Используемая политика сканирования
 
 // Инициализация ZAP
 print("Initializing ZAP...");
-var client = new org.zaproxy.clientapi.core.ClientApi("localhost", 80);
+var client = new org.zaproxy.clientapi.core.ClientApi("http://89.169.164.174", 80);
 
 // Запуск сканирования
 print("Starting active scan for: " + targetUrl);
